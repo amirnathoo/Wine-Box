52a45e0ac66bbf67306490a1d445c98c5dbca9b7
Amir Nathoo <amirnathoo@Amirs-MacBook-Air.local>
2012-04-26 23:40:44 -0700

    First rating page looks good

--- wine.js
+++ wine.js
@@ -1,3 +1,23 @@
+forge.debug = true;
+
+//Style top bar and tab bar
+
+//TODO - check when there is a fix in the platform
+/*forge.topbar.setTint([88,22,43,0]);
+forge.tabbar.setTint([88,22,43,0]); */
+
+//Fake support of :active on Android
+var fake_active = function(el) {
+	if (forge.is.android()) {
+		$(el).bind("touchstart", function () {
+			$(this).addClass("active");
+		}).bind("touchend", function() {
+			$(this).removeClass("active");
+		});
+	}
+}
+
+//Instrument tab bar for transitions
 var scrollTop = function () {
 	setTimeout(function () {
 		document.body.scrollTop = 0;
@@ -9,7 +29,7 @@ var updateView = function (search) {
 
 var starterButton = forge.tabbar.addButton({
 	text: "Rate Wine",
-	icon: "img/tomato.png",
+	icon: "img/star.png",
 	index: 0
 }, function (button) {
 	button.onPressed.addListener(function () {
@@ -23,7 +43,7 @@ var starterButton = forge.tabbar.addButton({
 
 var mainButton = forge.tabbar.addButton({
 	text: "My Wine",
-	icon: "img/pizza.png",
+	icon: "img/bottle.png",
 	index: 1
 }, function (button) {
 	button.onPressed.addListener(function () {
@@ -31,19 +51,6 @@ var mainButton = forge.tabbar.addButton({
 		forge.topbar.setTitle("My Wine");
 	});
 });
-var dessertButton = forge.tabbar.addButton({
-	text: "Wine Stream",
-	icon: "img/strawberry.png",
-	index: 2
-}, function (button) {
-	button.onPressed.addListener(function () {
-		updateView("public");
-		forge.topbar.setTitle("Wine Stream");
-	});
-});
-
-
-
 
 // Constants used for configuration
 var config = {
@@ -63,7 +70,7 @@ var state = {
 };
 
 // Organisation object
-var photolog = {
+var wine = {
 	types: {},
 	views: {},
 	models: {},
@@ -128,7 +135,7 @@ var setupNav = function () {
 	} else {
 		$('#prev-link').hide();
 	}
-	if(photolog.photos.length > state.pageNum * config.pageSize) {
+	if(wine.photos.length > state.pageNum * config.pageSize) {
 		$('#next-link').show();
 	} else {
 		$('#next-link').hide();
@@ -161,7 +168,7 @@ if (forge.is.ios()) {
 }
 
 // Router
-photolog.types.Router = Backbone.Router.extend({
+wine.types.Router = Backbone.Router.extend({
 	routes: {
 		"": "upload",
 		"upsell": "upsell",
@@ -177,7 +184,7 @@ photolog.types.Router = Backbone.Router.extend({
 		$('#scrollbox').hide();
 		$('#page-title').text('');
 
-		loading();
+		/*loading();
 		forge.request.ajax({
 			url: "https://api.parse.com/1/classes/Stream",
 			headers: {
@@ -204,7 +211,7 @@ photolog.types.Router = Backbone.Router.extend({
 			error: function () {
 				loaded();
 			}
-		});
+		});*/
 
 	},
 	stream: function (stream) {
@@ -216,9 +223,9 @@ photolog.types.Router = Backbone.Router.extend({
 			state.stream = stream;
 
 			// Remove current photos
-			photolog.photos.reset();
+			wine.photos.reset();
 			$('.loadedPhoto').remove();
-			photolog.util.update(setupNav);
+			wine.util.update(setupNav);
 		}
 		$('#photos').show();
 		$('#upload').remove();
@@ -237,11 +244,11 @@ photolog.types.Router = Backbone.Router.extend({
 			$('#upsell').show();
 			$('#streams').hide();
 		} else {
-			photolog.router.navigate('stream/'+state.stream, true);
+			wine.router.navigate('stream/'+state.stream, true);
 		}
 	},
 	upload: function () {
-		var page = new photolog.views.Upload();
+		var page = new wine.views.Upload();
 		page.render().show();
 	},
 	photo: function (stream, photoId) {
@@ -254,19 +261,19 @@ photolog.types.Router = Backbone.Router.extend({
 		if (state.stream != stream) {
 			state.stream = stream;
 		}
-		photolog.util.update(function() { photolog.util.getIndividualPhoto(photoId); });
+		wine.util.update(function() { wine.util.getIndividualPhoto(photoId); });
 	}
 });
-photolog.router = new photolog.types.Router();
+wine.router = new wine.types.Router();
 
 // Functions
-photolog.util = {
+wine.util = {
 	upload: function () {
-		photolog.router.navigate('upload', true);
+		wine.router.navigate('upload', true);
 	},
 	update: function (callback) {
 		loading();
-		forge.request.ajax({
+		/*forge.request.ajax({
 			url: "https://api.parse.com/1/classes/Photo",
 			headers: {
 				"X-Parse-Application-Id": config.parseAppId,
@@ -280,8 +287,8 @@ photolog.util = {
 			},
 			success: function (data) {
 				data.results.forEach(function (file) {
-					if (!photolog.photos.get(file.objectId)) {
-						photolog.photos.add([{
+					if (!wine.photos.get(file.objectId)) {
+						wine.photos.add([{
 							id: file.objectId,
 							url: file.file.url,
 							timestamp: Date.parse(file.createdAt.replace('T', ' ').replace('Z','').substring(0, file.createdAt.indexOf('.'))).getTime()
@@ -296,16 +303,16 @@ photolog.util = {
 			error: function () {
 				loaded();
 			}
-		});
+		});*/
 	},
 	getIndividualPhoto: function(photoId) {
-		var photo = photolog.photos.get(photoId);
-		state.index = photolog.photos.indexOf(photo);
+		var photo = wine.photos.get(photoId);
+		state.index = wine.photos.indexOf(photo);
 		$(document).keydown(function(e){
 		    if (e.keyCode == 37) {
-   	    		photolog.util.showIndividualPhoto(-1);
+   	    		wine.util.showIndividualPhoto(-1);
 		    } else if (e.keyCode == 39) {
-   	    		photolog.util.showIndividualPhoto(1);
+   	    		wine.util.showIndividualPhoto(1);
 		    }
 		});
 		$('#large-photo').attr('src', photo.get('url'));
@@ -319,15 +326,15 @@ photolog.util = {
 	    var nextPhoto = '';
 	    // A null state.index means show the 'Upsell' box instead of a photo
 		if (state.index == null) {
-			state.index = increment == 1 ? 0 : photolog.photos.length - 1;
-			nextPhoto = photolog.photos.at(state.index).get('url');
+			state.index = increment == 1 ? 0 : wine.photos.length - 1;
+			nextPhoto = wine.photos.at(state.index).get('url');
 		} else {
 	    	state.index += increment;
-	    	if (state.index == -1 || state.index == photolog.photos.length) {
+	    	if (state.index == -1 || state.index == wine.photos.length) {
 	    		state.index = null;
 	    	}
 			else {
-				nextPhoto = photolog.photos.at(state.index).get('url');
+				nextPhoto = wine.photos.at(state.index).get('url');
 			}
 	    }
 
@@ -354,188 +361,9 @@ photolog.util = {
 					duration: 200
 				});
 				state.animating = false;}});
-	}
-}
-
-// Models
-photolog.models.Photo = Backbone.Model.extend({
-});
-
-// Collections
-photolog.collections.Photos = Backbone.Collection.extend({
-	model: photolog.models.Photo,
-	comparator: function (model) {
-		return -model.get('timestamp');
-	}
-});
-photolog.photos = new photolog.collections.Photos();
-photolog.photos.on('add', function (model) {
-	var photo = new photolog.views.Photo({
-		model: model
-	});
-
-	var index = photolog.photos.indexOf(model);
-	loading();
-	var pageNum = Math.floor(index / config.pageSize) + 1;
-	if (index == 0) {
-		$('#header').after(photo.render(pageNum, false).el);
-	} else {
-		$(photolog.photos.at(index-1).get('el')).after(photo.render(pageNum, false).el);
-	}
-});
-
-// Views
-photolog.views.Upload = Backbone.View.extend({
-	tagName: "div",
-	id: "upload",
-	render: function() {
-		var el = this.el;
-		$(el).html('<div class="step"><div class="button" id="choosephoto">Choose photo</div><div class="label" style="position:absolute;top:20px;left:10px;font-size:26px;">1.</div></div>');
-		_.bindAll(this);
-		$('#uploadcancel', el).bind(clickEvent, this.cancel);
-		$('#choosephoto', el).bind(clickEvent, this.choose);
-		$('#uploadphoto', el).bind(clickEvent, this.upload);
-		return this;
 	},
-	show: function () {
-		$('body').append(this.el);
-	},
-	cancel: function () {
-		photolog.router.navigate('stream/'+state.stream, true);
-	},
-	upload: function () {
-		if (!this.selImage) {
-			alert("Please choose a photo to upload first!");
-		} else {
-			var stream = $('#streamid').val().replace(/#/g, '') || state.stream || config.defaultStream;
-			photolog.router.navigate('stream/'+stream, true);
-			loading();
-			forge.request.ajax({
-				url: "https://api.parse.com/1/files/"+(new Date()).getTime()+".jpg",
-				headers: {
-					"X-Parse-Application-Id": config.parseAppId,
-					"X-Parse-REST-API-Key": config.parseRestKey
-				},
-				type: "POST",
-				files: [this.selImage],
-				fileUploadMethod: 'raw',
-				dataType: 'json',
-				success: function (data) {
-					forge.request.ajax({
-						url: "https://api.parse.com/1/classes/Photo",
-						headers: {
-							"X-Parse-Application-Id": config.parseAppId,
-							"X-Parse-REST-API-Key": config.parseRestKey
-						},
-						type: "POST",
-						contentType: "application/json",
-						dataType: 'json',
-						data: JSON.stringify({
-							file: {
-								"__type": "File",
-								name: data.name
-							},
-							location: state.location,
-							stream: stream
-						}),
-						success: function (file) {
-							loaded();
-
-							// Tweet
-							forge.tabs.open("https://twitter.com/share?url=" + encodeURIComponent("http://eventstream.io/#photo/"+file.objectId) + "&text=" + encodeURIComponent("Just posted to Trigger Photolog #"+stream+" - "));
-
-							photolog.photos.add([{
-								id: file.objectId,
-								url: data.url,
-								timestamp: Date.parse(file.createdAt.replace('T', ' ').replace('Z','').substring(0, file.createdAt.indexOf('.'))).getTime()
-							}]);
-						}, error: function () {
-							loaded();
-						}
-					});
-
-				}, error: function () {
-					loaded();
-				}
-			});
-
-			// Update stream
-			forge.request.ajax({
-				url: "https://api.parse.com/1/classes/Stream",
-				headers: {
-					"X-Parse-Application-Id": config.parseAppId,
-					"X-Parse-REST-API-Key": config.parseRestKey
-				},
-				type: "GET",
-				dataType: 'json',
-				data: {
-					"where": '{"stream": "'+state.stream+'"}',
-					"limit": 1
-				},
-				success: function (data) {
-					if (data.results.length > 0) {
-						// PUT requests don't work on all platforms right now
-						// Update stream date
-						forge.request.ajax({
-							url: "https://api.parse.com/1/classes/Stream/"+data.results[0].objectId,
-							headers: {
-								"X-Parse-Application-Id": config.parseAppId,
-								"X-Parse-REST-API-Key": config.parseRestKey
-							},
-							type: "PUT",
-							contentType: "application/json",
-							dataType: 'json',
-							data: '{}'
-						});
-					} else {
-						// Create stream
-						forge.request.ajax({
-							url: "https://api.parse.com/1/classes/Stream",
-							headers: {
-								"X-Parse-Application-Id": config.parseAppId,
-								"X-Parse-REST-API-Key": config.parseRestKey
-							},
-							type: "POST",
-							contentType: "application/json",
-							dataType: 'json',
-							data: JSON.stringify({
-								stream: stream
-							})
-						});
-					}
-				}
-			});
-
-		}
-	},
-	choose: function () {
-		var self = this;
-		self.selImage = undefined;
-		$('.toUpload').remove();
-		forge.file.getImage({width: 500, height: 500}, function (file) {
-			forge.file.imageURL(file, function (url) {
-				var photo = new photolog.views.Photo({
-					model: new photolog.models.Photo({url: url})
-				});
-				$('#upload').after(photo.render(null, true).el);
-				$(photo.el).addClass("toUpload");
-			});
-			self.selImage = file;
-		});
-	}
-});
-
-photolog.views.Photo = Backbone.View.extend({
-	tagName: "div",
-	className: "photo",
-
-	render: function(pageNum, isUpload) {
-		var el = this.el;
-		this.model.set('el', el);
-		$(el).hide();
-		var preloadImage = new Image();
-		var photoId = this.model.get('id');
-		preloadImage.onload = function () {
+	showPhoto: function(el, preloadImage, target) { 
+		return function() {
 			var square, heightOffset, widthOffset;
 			if (preloadImage.height > preloadImage.width) {
 				square = preloadImage.width;
@@ -546,15 +374,91 @@ photolog.views.Photo = Backbone.View.extend({
 				widthOffset = (preloadImage.width - square) / 2;
 				heightOffset = 0;
 			}
-			var target = 220;
 			var ratio = target/square;
-			$(el).html('<div style="display: inline-block; height: '+square*ratio+'px; width: '+square*ratio+'px; overflow: hidden"><span class="a" data-href="#photo/'+state.stream+'/' +photoId+'"><img style="width: '+preloadImage.width*ratio+'px; height: '+preloadImage.height*ratio+'px; margin-left: -'+widthOffset*ratio+'px; margin-top: -'+heightOffset*ratio+'px" src="'+preloadImage.src+'"></span></div>');
-			if (pageNum == 1 || isUpload) {
-				$(el).fadeIn('slow');
-			}
-			$(el).addClass('loadedPhoto photoPage' + pageNum);
-			loaded();
+			$(el).append('<div class="photowrapper" style="height: '+square*ratio+'px; width: '+square*ratio+'px; overflow: hidden"></div>');
+			var style = 'width: '+preloadImage.width*ratio+'px; height: '+preloadImage.height*ratio+'px; margin-left: -'+widthOffset*ratio+'px; margin-top: -'+heightOffset*ratio+'px';
+			$('.photowrapper', el).append(preloadImage);
+			$(preloadImage, el).attr('style', style);
 		}
+	}
+}
+
+// Models
+wine.models.Photo = Backbone.Model.extend({
+});
+
+// Collections
+wine.collections.Photos = Backbone.Collection.extend({
+	model: wine.models.Photo,
+	comparator: function (model) {
+		return -model.get('timestamp');
+	}
+});
+wine.photos = new wine.collections.Photos();
+wine.photos.on('add', function (model) {
+	var photo = new wine.views.Photo({
+		model: model
+	});
+
+	var index = wine.photos.indexOf(model);
+	loading();
+	var pageNum = Math.floor(index / config.pageSize) + 1;
+	if (index == 0) {
+		$('#header').after(photo.render(pageNum, false).el);
+	} else {
+		$(wine.photos.at(index-1).get('el')).after(photo.render(pageNum, false).el);
+	}
+});
+
+// Views
+wine.views.Upload = Backbone.View.extend({
+	tagName: "div",
+	id: "upload",
+	render: function() {
+		var el = this.el;
+		$(el).html('<div id="choosephoto" class="step"><div class="label">1.</div><div class="instruction">Take picture of label</div><img class="icon"></img></div>');
+		forge.tools.getURL('img/disclosure_indicator.png', function(src) {
+			$('img.icon', el).attr('src', src);
+		});
+		forge.tools.getURL('img/wine.jpg', function(src) {
+			$(el).attr('style', 'width:100%;height:100%;background-image:url('+src+');background-size:cover;');
+		});
+		_.bindAll(this);
+		$('#choosephoto', el).bind(clickEvent, this.choose);
+		fake_active($('#choosephoto', el));
+		return this;
+	},
+	show: function () {
+		$('body').append(this.el);
+	},
+	choose: function () {
+		var self = this;
+		self.selImage = undefined;
+		$('.toUpload').remove();
+		forge.file.getImage({width: 500, height: 500}, function (file) {
+			forge.file.imageURL(file, function (url) {
+				var photo = new wine.views.Photo({
+					model: new wine.models.Photo({url: url})
+				});
+			});
+			self.selImage = file;
+			$(self.el).html('<div class="step"><div class="label">2.</div><div class="instruction">Rate wine</div></div>');
+			alert('amirtest');
+		});
+	}
+});
+
+wine.views.Photo = Backbone.View.extend({
+	tagName: "div",
+	className: "photo",
+
+	render: function(pageNum, isUpload) {
+		var el = this.el;
+		this.model.set('el', el);
+		$(el).hide();
+		var preloadImage = new Image();
+		var photoId = this.model.get('id');
+		preloadImage.onload = wine.util.showPhoto;
 		preloadImage.src = this.model.get('url');
 		return this;
 	}
@@ -563,39 +467,4 @@ photolog.views.Photo = Backbone.View.extend({
 // Initialise app
 $(function () {
 	Backbone.history.start()
-	if (forge.is.web()) {
-		$('#upload-container').hide();
-		// Make mobile web experience the same as desktop web
-		// if (navigator.userAgent.indexOf('Android') != -1) {
-		// 	$('#appstore').hide();
-		// 	photolog.router.navigate('upsell', true);
-		// } else if (navigator.userAgent.indexOf('iPhone') != -1 || navigator.userAgent.indexOf('iPad') != -1) {
-		// 	$('#androidmarket').hide();
-		// 	photolog.router.navigate('upsell', true);
-		// }
-	} else {
-		$('#app-download-container').hide();
-		$('#footer-links').hide();
-	}
-	$('#index-button').bind(clickEvent, function(e) {
-		e.preventDefault();
-		photolog.router.navigate("/", {trigger: true});
-	});
-	$('#plus-button').bind(clickEvent, function(e) {
-		e.preventDefault();
-	});
-	$('#upload-button').bind(clickEvent, function(e) {
-		e.preventDefault();
-		photolog.util.upload();
-	})
-	if (forge.is.ios()) {
-		$('.top-bar').each(function() {$(this).addClass('top-bar-iphone')});
-	} else {
-		$('.top-bar').each(function() {$(this).addClass('top-bar-android')});
-	}
-
-	// Check for photos then poll every 10 seconds, bit hacky.
-	setInterval(function () {
-		photolog.util.update();
-	}, 10000);
 });
